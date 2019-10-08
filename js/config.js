7b3de2c940012e6a99dc0f5ff4ca0f548a785f8e
Greg Eckhart <greg@gregs-mbp.attlocal.net>
2019-10-08 17:01:34 -0400

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_H5Mb1yQwv', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '72fq0vgih9ao64v0nmnvoemd78', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
